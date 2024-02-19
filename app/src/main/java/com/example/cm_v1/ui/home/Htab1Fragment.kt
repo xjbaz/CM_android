4f1a1dc13b170ef280f7f6b9bb1b91852c7c9f6f
インターン用 <>
2024-02-19 19:36:37 +0900

    画像リスト調整

--- app/src/main/java/com/example/cm_v1/ui/home/Htab1Fragment.kt
+++ app/src/main/java/com/example/cm_v1/ui/home/Htab1Fragment.kt
@@ -45,16 +45,25 @@ class Htab1Fragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // データを用意
-        val number = arrayOf("とり", "しか", "きつね", "かば", "ライオン", "パンダ", "ひつじ")
+        val number = arrayOf(
+            "個体番号1", "個体番号2", "個体番号3", "個体番号4", "個体番号5", "個体番号6",
+            "個体番号7", "個体番号8", "個体番号9", "個体番号10", "個体番号11", "個体番号12",
+            "個体番号13", "個体番号14", "個体番号15", "個体番号16", "個体番号17", "個体番号18",
+        )
         val details = arrayOf(
-            "「とり」の説明文が入ります。", "「しか」の説明文が入ります。",
-            "「きつね」の説明文が入ります。", "「かば」の説明文が入ります。",
-            "「ライオン」の説明文が入ります。", "「パンダ」の説明文が入ります。",
-            "「ひつじ」の説明文が入ります。"
+            "ステータス1","ステータス2","ステータス3",
+            "ステータス4","ステータス5","ステータス6",
+            "ステータス7","ステータス8","ステータス9",
+            "ステータス10","ステータス11","ステータス12",
+            "ステータス13","ステータス14","ステータス15",
+            "ステータス16","ステータス17","ステータス18",
         )
         val images = intArrayOf(
             R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4,
-            R.drawable.img5, R.drawable.img6, R.drawable.img7
+            R.drawable.img5, R.drawable.img6, R.drawable.img7, R.drawable.img8,
+            R.drawable.img9, R.drawable.img10, R.drawable.img11, R.drawable.img12,
+            R.drawable.img13, R.drawable.img14, R.drawable.img15, R.drawable.img16,
+            R.drawable.img17, R.drawable.img18,
         )
 
         val listData = ArrayList<Map<String, Any>>()
@@ -72,7 +81,7 @@ class Htab1Fragment : Fragment() {
             context,
             listData,
             R.layout.list_item1,
-            arrayOf("name", "detail", "image"),
+            arrayOf("number", "detail", "image"),
             intArrayOf(R.id.number, R.id.detail, R.id.image)
         )
     }
