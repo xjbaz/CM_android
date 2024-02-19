a15c97a93c0a4da3c1160c0d6245fa260889d888
インターン用 <>
2024-02-19 17:33:43 +0900

    ホームのタブ追加

--- app/src/main/java/com/example/cm_v1/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cm_v1/ui/home/HomeFragment.kt
@@ -7,7 +7,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.cm_v1.R
 import com.example.cm_v1.databinding.FragmentHomeBinding
+import com.example.cm_v1.ui.dashboard.Dtab1Fragment
+import com.example.cm_v1.ui.dashboard.Dtab2Fragment
+import com.example.cm_v1.ui.dashboard.Dtab3Fragment
+import com.example.cm_v1.ui.dashboard.Dtab4Fragment
+import com.google.android.material.tabs.TabLayout
 
 class HomeFragment : Fragment() {
 
@@ -17,22 +23,49 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupTabLayout()
+    }
+
+
+
+
+    private fun setupTabLayout() {
+        binding.tabs2.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> showFragment(HtabAllFragment())
+                    1 -> showFragment(Htab1Fragment())
+                    2 -> showFragment(Htab2Fragment())
+                    3 -> showFragment(Htab3Fragment())
+                    4 -> showFragment(Htab4Fragment())
+                    5 -> showFragment(Htab5Fragment())
+                }
+            }
+
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // ここにコードは不要です。
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // ここにコードは不要です。
+            }
+        })
+
+        // 初期フラグメントを設定
+        showFragment(HtabAllFragment())
+    }
+    private fun showFragment(fragment: Fragment) {
+        childFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 
     override fun onDestroyView() {
