4588381ba5f38a14da847b7590b39d86c8aa382b
インターン用 <>
2024-02-28 18:05:39 +0900

    一旦

---
+++ app/src/main/java/com/example/cm_v1/ui/info/InfoFragment.kt
@@ -0,0 +1,79 @@
+package com.example.cm_v1.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.cm_v1.R
+import com.example.cm_v1.databinding.FragmentHomeBinding
+import com.example.cm_v1.ui.dashboard.Dtab1Fragment
+import com.example.cm_v1.ui.dashboard.Dtab2Fragment
+import com.example.cm_v1.ui.dashboard.Dtab3Fragment
+import com.example.cm_v1.ui.dashboard.Dtab4Fragment
+import com.example.cm_v1.ui.info.Itab1Fragment
+import com.example.cm_v1.ui.info.Itab2Fragment
+import com.example.cm_v1.ui.info.Itab3Fragment
+import com.example.cm_v1.ui.info.Itab4Fragment
+import com.example.cm_v1.ui.info.Itab5Fragment
+import com.google.android.material.tabs.TabLayout
+
+class InfoFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupTabLayout()
+    }
+
+
+
+
+    private fun setupTabLayout() {
+        binding.tabs2.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> showFragment(Itab1Fragment())
+                    1 -> showFragment(Itab2Fragment())
+                    2 -> showFragment(Itab3Fragment())
+                    3 -> showFragment(Itab4Fragment())
+                    4 -> showFragment(Itab5Fragment())
+                }
+            }
+
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // ここにコードは不要です。
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // ここにコードは不要です。
+            }
+        })
+
+        // 初期フラグメントを設定
+        showFragment(HtabAllFragment())
+    }
+    private fun showFragment(fragment: Fragment) {
+        childFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/example/cm_v1/ui/info/Itab2Fragment.kt
+++ app/src/main/java/com/example/cm_v1/ui/info/Itab2Fragment.kt
@@ -1,4 +1,4 @@
-package com.example.cm_v1.ui.setting
+package com.example.cm_v1.ui.info
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SettingFragment.newInstance] factory method to
+ * Use the [Itab2Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SettingFragment : Fragment() {
+class Itab2Fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        return inflater.inflate(R.layout.fragment_itab2, container, false)
     }
 
     companion object {
@@ -45,17 +45,16 @@ class SettingFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingFragment.
+         * @return A new instance of fragment Itab2Fragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SettingFragment().apply {
+            Itab2Fragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
     }
-
 }
