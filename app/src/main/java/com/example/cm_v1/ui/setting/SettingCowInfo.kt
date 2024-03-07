5652714e61566f22b58fc478072ba90d5bcdca6a
k_fukushima <k_fukushima@eyemovic.com>
2024-03-07 15:31:44 +0900

    個体情報画面の作成

--- app/src/main/java/com/example/cm_v1/ui/setting/SettingCowInfo.kt
+++ app/src/main/java/com/example/cm_v1/ui/setting/SettingCowInfo.kt
@@ -2,21 +2,72 @@ package com.example.cm_v1
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import java.util.*
 
 class SettingCowInfo : AppCompatActivity() {
     private lateinit var cowInfo: Button
+    private lateinit var editText: EditText
+    private lateinit var edit_info_date: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.setting_cow_info)
 
         // ボタンをレイアウトファイルから取得
-        cowInfo = findViewById(R.id.btnBack)
+        edit_info_date = findViewById(R.id.edit_info_date)
+        edit_info_date.setOnClickListener {
+            showDatePickerDialog()
+        }
+
+        val radioGroup: RadioGroup = findViewById(R.id.radio_gender)
+        val button1: RadioButton = findViewById(R.id.radio_gender_osu)
+        val button2: RadioButton = findViewById(R.id.radio_gender_mesu)
+
+// ラジオボタングループの選択状態が変更されたときのリスナーを設定
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            // ラジオボタンが選択されたときの処理
+            when (checkedId) {
+                R.id.radio_gender_osu -> {
+                    // Button 1が選択されたときの処理
+                    button2.isChecked = false // Button 2の選択を解除
+                }
+                R.id.radio_gender_mesu -> {
+                    // Button 2が選択されたときの処理
+                    button1.isChecked = false // Button 1の選択を解除
+                }
+            }
+        }
+
+        cowInfo = findViewById(R.id.btnFinish)
+        editText = findViewById(R.id.edit_info_name)
+        editText = findViewById(R.id.edit_info_number)
 
         // ボタンにクリックリスナーを設定
         cowInfo.setOnClickListener {
             finish()
         }
+    }
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, selectedYear, selectedMonth, selectedDay ->
+                // 日付が選択されたときの処理
+                val selectedDate = "$selectedYear/${selectedMonth + 1}/$selectedDay"
+                edit_info_date.setText(selectedDate)
+            },
+            year, month, day
+        )
+
+        datePickerDialog.show()
     }
 }
