c7bbb7ce720ab8ddd1840606770d8a7fd6f502c1
k_fukushima <k_fukushima@eyemovic.com>
2024-04-11 17:15:11 +0900

    とってきたやつ

---
+++ app/src/main/java/com/example/cm_v1/ui/home/DialogHomeFragment.kt
@@ -0,0 +1,36 @@
+// YesNoDialogFragment.kt
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+import com.example.cm_v1.LoginActivity
+import com.example.cm_v1.R
+
+class DialogHomeFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(requireContext())
+        val inflater = requireActivity().layoutInflater
+        val view = inflater.inflate(R.layout.dialog_layout, null)
+
+
+        builder.setView(view)
+
+        // ダイアログ内のYesボタンがクリックされたときの処理
+        view.findViewById<Button>(R.id.buttonYes).setOnClickListener {
+            // ここにYesボタンがクリックされたときの処理を記述
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        // ダイアログ内のNoボタンがクリックされたときの処理
+        view.findViewById<Button>(R.id.buttonNo).setOnClickListener {
+            // ここにNoボタンがクリックされたときの処理を記述
+            dismiss() // ダイアログを閉じる
+        }
+
+        return builder.create()
+    }
+}
